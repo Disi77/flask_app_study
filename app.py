7b6361be589d90d560293bd0ce53c3981252a983
Disi77 <disi77@gmail.com>
2023-03-07 23:42:00 +0100

    second commit

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, SubmitField, SelectField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, NumberRange
 
 
 app = Flask(__name__)
 app.secret_key = "lasjkflksdfjalskfj"
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    form = OrderForm()
-    
-    if form.validate_on_submit():
-        first_name = form.first_name.data
-        last_name = form.last_name.data
-        email = form.email.data
-        print(first_name, last_name, email)
-
-
-    return render_template("index.html", form=form)
-
-
-# set FLASK_DEBUG=on
 
 
 class OrderForm(FlaskForm):
     first_name = StringField("First name", validators=[DataRequired(), Length(min=2, max=55)])
     last_name = StringField("Last name", validators=[DataRequired(), Length(min=2, max=55)])
     email = StringField("Email", validators=[DataRequired(), Email()])
+    product = SelectField("Favorite product", choices=[('p1', 'Product 1 (500 Kč)'), ('p2', 'Product 2 (1.000 Kč)'), ('p3', 'Product 3 (1.500 Kč)')])
+    quantity = IntegerField("Quantity", validators=[DataRequired(), NumberRange(min=1, max=20)])
+    submit = SubmitField("Submit1")
 
-    submit = SubmitField("Convert2")
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    form = OrderForm()
+    print(1)
+    if form.is_submitted():
+        print("druhá kontrola")
+
+    if form.validate_on_submit():
+        print("kontrola")
+        # first_name = form.first_name.data
+        # last_name = form.last_name.data
+        # email = form.email.data
+        # print(first_name, last_name, email)
+
+    print(2)
+    return render_template("index.html", form=form)
+
+
+# set FLASK_DEBUG=on
