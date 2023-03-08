e0a99166604bf83a727f97d0f4514507e9685eca
Disi77 <disi77@gmail.com>
2023-03-08 16:11:32 +0100

    čtvrtý commit

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+function setValue(from, to) {
+    var toElement = document.getElementById(to);
+    var fromElement = document.getElementById(from);
+    toElement.innerHTML = fromElement.value * 100;
+};
+
+var quantity = document.getElementById("quantity");
+quantity.onchange = function () {
+    setValue("quantity", "result");
+};
+
+window.onload = function () {
+    setValue("quantity", "result");
+};
+
